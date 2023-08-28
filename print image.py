ae27976c4b714e3c28dde3df04146d4d5a210287
efrat <efrat.barak02@gmail.com>
2023-08-28 16:48:23 +0300

    added def print images to functions

---
+++ print image.py
@@ -0,0 +1,14 @@
+import pygame
+import consts
+
+arrow = pygame.image.load('flag.png')
+sized_arrow = pygame.transform.scale(arrow, (
+    consts.WIDTH, consts.HEIGHT))
+
+# Create a box to put the arrow in, so that the rotation will be around
+# it's bottom (the box's center)
+arrow_box = pygame.Surface(
+    (consts.WIDTH, consts.HEIGHT * 2), )
+arrow_box.fill(consts.WHITE)
+arrow_box.blit(sized_arrow, (0, 0))
+print(arrow_box)
