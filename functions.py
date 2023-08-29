c5ef3318c2a67e75fccbd8bca11255e0249019a5
adielishoov <adielishoov@gmail.com>
2023-08-29 10:52:56 +0300

    new fun

--- functions.py
+++ functions.py
@@ -2,6 +2,17 @@ import consts
 import random
 
 
+def trans_index_to_cordinata(list_of_index):
+    list_of_cord = []
+    for set_of_index in list_of_index:
+        cordinata_x = set_of_index[0] * (20 + 1)
+        cordinata_y = set_of_index[1] * (20 + 1)
+        set_of_cord = (cordinata_x, cordinata_y)
+        list_of_cord.append(set_of_cord)
+
+    return list_of_cord
+
+
 def creating_matrix():
     matrix = []
     for hours in range(25):
@@ -9,6 +20,7 @@ def creating_matrix():
         for days in range(50):
             new.append("empty")
         matrix.append(new)
+    add_grass(matrix)
     return matrix
 
 
@@ -30,4 +42,8 @@ def add_grass(matrix):
             index_grass = [rand_rwo, rand_col]
             list_of_grass.append(tuple(index_grass))
             num_grass += 1
+    trans_index_to_cordinata(list_of_grass)
     return list_of_grass
+
+
+creating_matrix()
